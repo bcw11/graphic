f8117532c9b7c120e52570f4f4f8535b1e520573
Bryan Wong <bryan.adams1001@gmail.com>
2026-05-07 17:44:34 -0700

    greyscale image

--- graphic.py
+++ graphic.py
@@ -1,27 +1,21 @@
 import cv2
-
-
+import numpy as np
 
 def main():
 
     img = cv2.imread("Google.png")
     
     h, w, c = img.shape 
-    colors = set()
     print(img[0][0])
     for i in range(h):
         for j in range(w):
-            strcolor = str(img[i][j][0])+","+str(img[i][j][1])+","+str(img[i][j][2])
-            if strcolor not in colors:
-                print(strcolor)
-                colors.add(strcolor)
-                
-    print(colors)
+            color = max(200*round(int(img[i][j][0])/255), 20)
+            img[i][j] = np.array([color, color, color])
 
 
-    # cv2.imshow("win", img)               # display
-    # cv2.waitKey(0)                       # wait for key
-    # cv2.destroyAllWindows()              # close window
+    cv2.imshow("win", img)               # display
+    cv2.waitKey(0)                       # wait for key
+    cv2.destroyAllWindows()              # close window
 
     cv2.imwrite("out.jpg", img)   
 
