0607b0c7be133c3fddf9938764c17cae1afacab0
bcw11 <bcw11@sfu.ca>
2026-05-07 17:17:14 -0700

    playing around

--- graphic.py
+++ graphic.py
@@ -1,16 +1,31 @@
-import os 
-import sys 
-
-
-
+import cv2
 
 
 
 def main():
 
+    img = cv2.imread("Google.png")
+    
+    h, w, c = img.shape 
+    colors = set()
+    print(img[0][0])
+    for i in range(h):
+        for j in range(w):
+            strcolor = str(img[i][j][0])+","+str(img[i][j][1])+","+str(img[i][j][2])
+            if strcolor not in colors:
+                print(strcolor)
+                colors.add(strcolor)
+                
+    print(colors)
 
 
-    None 
+    # cv2.imshow("win", img)               # display
+    # cv2.waitKey(0)                       # wait for key
+    # cv2.destroyAllWindows()              # close window
+
+    cv2.imwrite("out.jpg", img)   
+
+     
 
 if __name__ == "__main__":
     main()
