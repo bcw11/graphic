1bafc150483d0b6d4b327da295ce559f20949134
Bryan Wong <bryan.adams1001@gmail.com>
2026-05-11 15:01:23 -0700

    changes

--- graphic.py
+++ graphic.py
@@ -3,8 +3,42 @@ import cupy as cp
 import numpy as np
 import time
 
+def grey_dither(img):
+    h, w, c = img.shape 
+    max_light = 220
+    min_dark = 20
 
-def grey_dither(img_np):
+    # greyscale image
+    for i in range(h):
+        for j in range(w):
+            color = max(max_light*round(int(img[i][j][0])/255), min_dark)
+            img[i][j] = np.array([color]*3)
+
+
+    # # adding noise
+    # for i in range(h):
+    #     for j in range(w):
+    #         noise_offset = max(int(img[i][j][0]) + int(20 * random.uniform(-1, 1)), 0)
+    #         img[i][j] += noise_offset
+
+    # bayer = np.array([
+    #         [0.125, 0.625],
+    #         [0.875, 0.375]
+    #     ])  # threshold matrix; you can increase size (4x4, 8x8) if you want
+    # for i in range(h):
+    #     for j in range(w):
+    #         # tile the 2x2 matrix over the whole image
+    #         thr = bayer[i % 2, j % 2]
+    #         for k in range(c):
+
+    #             if img[i, j, k]/256 >= thr:
+    #                 img[i, j, k] = 255.0   # high level
+    #             else:
+    #                 img[i, j, k] = 0.0   # low level
+
+    return img
+
+def grey_dither_gpu(img_np):
     img = cp.asarray(img_np, dtype=cp.float32) / 255.0
     h, w, c = img.shape 
     max_light = 220
